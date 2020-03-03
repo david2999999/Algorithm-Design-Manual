66943d8ac3848bf2d0a20710136fe17e5543c6b7
david2999999 <djiang86@binghamton.edu>
2020-03-03 09:03:35 -0500

    searching/delete node from a linked list

--- Chapter 3 - Data Structures/Singly Linked List/SinglyLinkedList.c
+++ Chapter 3 - Data Structures/Singly Linked List/SinglyLinkedList.c
@@ -27,3 +27,33 @@ void insert_list(list **l, item_type x) {
     p -> next = *l;
     *l = p;
 }
+
+list *predecessor_list(list *l, item_type x) {
+    if ((l == NULL) || (l -> next == NULL)) {
+        return NULL;
+    }
+
+    if ((l -> next) -> item == x) {
+        return l;
+    } else {
+        return predecessor_list(l -> next, x);
+    }
+}
+
+delete_list(list **l, item_type x) {
+    list *p;
+    list *pred;
+    list *search_list(), *predecessor_list();
+
+    p = search_list(*l, x);
+    if (p != NULL) {
+        pred = predecessor_list(*l, x);
+        if (pred == NULL) {
+            *l = p -> next;
+        } else {
+            pred -> next = p -> next;
+        }
+
+        free(p);
+    }
+}

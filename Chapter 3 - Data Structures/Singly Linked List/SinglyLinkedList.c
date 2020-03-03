ef7c1422b35df32c2043010773e8e95536a5a65a
david2999999 <djiang86@binghamton.edu>
2020-03-03 08:41:20 -0500

    inserting element to the front of linked list

--- Chapter 3 - Data Structures/Singly Linked List/SinglyLinkedList.c
+++ Chapter 3 - Data Structures/Singly Linked List/SinglyLinkedList.c
@@ -3,8 +3,6 @@ typedef struct list {
     struct list *next;
 } list;
 
-// We use *p to denote the item that is pointed to by pointer p
-
 list *search_list(list *l, item_type x) {
     if (l == NULL) {
         return NULL;
@@ -15,4 +13,17 @@ list *search_list(list *l, item_type x) {
     } else {
         return search_list(l -> next, x);
     }
+}
+
+//  Two C-isms to note. First, the malloc function allocates a chunk of memory
+//   of sufficient size for a new node to contain x. Second, the funny double star (**l)
+//   denotes that l is a pointer to a pointer to a list node. Thus the last line, *l=p;
+//   copies p to the place pointed to l, which is the external variable maintaining access
+//   to the head of the list.
+void insert_list(list **l, item_type x) {
+    list *p;
+    p = malloc(sizeof(list));
+    p -> item = x;
+    p -> next = *l;
+    *l = p;
 }

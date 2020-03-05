151832a3757ba93ece18d0a6ffe42b246a6fd52c
david2999999 <djiang86@binghamton.edu>
2020-03-05 08:33:17 -0500

    searching values in binary search tree

--- Chapter 3 - Data Structures/Binary Search Tree/BinarySearchTree.c
+++ Chapter 3 - Data Structures/Binary Search Tree/BinarySearchTree.c
@@ -4,3 +4,17 @@ typedef struct tree {
     struct tree *left;
     struct tree *right;
 } tree;
+
+tree *search_tree(tree *t, item_type x) {
+    if (t == NULL) {
+        return NULL;
+    }
+
+    if (t -> item == x) {
+        return t;
+    } else if (t -> item > x) {
+        return search(t -> left, x);
+    } else {
+        return search(t -> right, x);
+    }
+}
